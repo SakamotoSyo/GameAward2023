a4e7415c58b5760283b149ac1834cfe06b65c8b3
Akira Watanabe <a0323w@gmail.com>
2023-03-08 14:44:16 +0900

    [add] 武器のステータス

---
+++ Assets/Personal/Watanabe/StatusView.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusView : MonoBehaviour
+{
+    [SerializeField] private Text _attackValue = default;
+    [SerializeField] private Text _criticalValue = default;
+    [SerializeField] private Text _testValue = default;
+
+    private WeaponStatus _status = default;
+
+    private void Start()
+    {
+        _status = GetComponent<WeaponStatus>();
+    }
+
+    private void Update()
+    {
+        _attackValue.text = $"Attack : {_status.AttackValue}";
+        _criticalValue.text = $"Critical : {_status.CriticalValue}";
+        _testValue.text = $"Test : {_status.TestValue}";
+    }
+}
