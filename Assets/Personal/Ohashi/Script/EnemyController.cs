ae23e14189bc43efd26c28a3fe927ff0c1a27447
RK <riku.kuu0421@gmail.com>
2023-03-08 16:20:32 +0900

    [fix]EnemyControllerを修正

--- Assets/Personal/Ohashi/Script/EnemyController.cs
+++ Assets/Personal/Ohashi/Script/EnemyController.cs
@@ -12,31 +12,19 @@ public class EnemyController : MonoBehaviour, IAddDamage
 
     public IReadOnlyReactiveProperty<float> Health => _health;
 
-
     private void Start()
     {
         _health.Value = _maxHealth;
-        HealthObserver();
-    }
-
-    /// <summary>
-    /// healthの値を監視し、0以下になった時にSubscribeする
-    /// </summary>
-    private void HealthObserver()
-    {
-        _health
-            .Where(health => health <= 0)
-            .Subscribe(_ =>
-            {
-                Debug.Log("Enemyの体力が0になった");
-                gameObject.SetActive(false);
-            })
-            .AddTo(this);
     }
 
     public void AddDamage(float damage)
     {
         _health.Value -= damage;
         Debug.Log($"Enemyは{damage}ダメージ受けた");
+        if(_health.Value <= 0)
+        {
+            Debug.Log("Enemyの体力が0になった");
+            gameObject.SetActive(false);
+        }
     }
 }
