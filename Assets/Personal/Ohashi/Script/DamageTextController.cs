6b40dab5b542ef9bf516b7694630d525fe7b79ba
RK <riku.kuu0421@gmail.com>
2023-03-09 15:17:38 +0900

    [update]ダメージアニメーション追加

--- Assets/Personal/Ohashi/Script/DamageTextController.cs
+++ Assets/Personal/Ohashi/Script/DamageTextController.cs
@@ -16,19 +16,40 @@ public class DamageTextController : MonoBehaviour
     [SerializeField, Tooltip("フェード時間")]
     private float _fadeTime = 0.5f;
 
+    private int _damage;
+
     private void Start()
     {
-        transform.DOLocalMoveY(_distanceY, _distanceYTime);
-        _damageText.DOFade(0, _fadeTime)
-            .SetDelay(_distanceYTime)
-            .OnKill(() => Destroy(gameObject));
+        //仮の条件
+        if(_damage >= 100)
+        {
+            _damageText.DOCounter(0, _damage, 0.2f);
+            transform.DOScale(new Vector3(0.02f, 0.02f, 0), 0.2f);
+            DamageAnimation();
+        }
+        else
+        {
+            DamageAnimation();
+        }
     }
 
     /// <summary>
     /// テキストにダメージを表示する
     /// </summary>
-    public void TextInit(float damage)
+    public void TextInit(int damage)
     {
         _damageText.text = damage.ToString();
+        _damage = damage;
+    }
+
+    /// <summary>
+    /// ダメージテキストのアニメーション
+    /// </summary>
+    private void DamageAnimation()
+    {
+        transform.DOLocalMoveY(_distanceY, _distanceYTime);
+        _damageText.DOFade(0, _fadeTime)
+            .SetDelay(_distanceYTime)
+            .OnComplete(() => Destroy(gameObject));
     }
 }
