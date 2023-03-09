71a20342256598d6dcf8f1200e5e464ea613673d
Akira Watanabe <a0323w@gmail.com>
2023-03-09 15:05:40 +0900

    [add] 武器（攻撃）

--- Assets/Personal/Watanabe/Scripts/WeaponStatus.cs
+++ Assets/Personal/Watanabe/Scripts/WeaponStatus.cs
@@ -3,61 +3,85 @@ using UnityEngine;
 
 public class WeaponStatus : MonoBehaviour
 {
+    [SerializeField] private AttackType _type = AttackType.Normal;
     [SerializeField] private SkillName _skillName;
     [SerializeField] private SkillDataBase _skillData;
 
     [Header("武器のステータス一覧")]
-    [SerializeField] private List<float> _values = new();
+    [SerializeField] private List<int> _values = new();
     [Tooltip("上昇値")]
-    [SerializeField] private float _updateValue = 1f;
+    [SerializeField] private int _updateValue = 1;
     [Tooltip("会心の確率(5～50)")]
     [Range(5, 50)]
     [SerializeField] private int _probCritical = 10;
 
-    private bool[] _updating = new bool[] {false, false, false};
+    private readonly List<int> _defaultValue = new();
+    private readonly bool[] _updating = new bool[] {false, false, false};
 
     //以下UI表示用
-    public float AttackValue
+    public int AttackValue
     { 
         get => _values[0];
         protected set => _values[0] = value;
     }
-    public float CriticalValue
+    public int TestValue
     {
         get => _values[1];
         protected set => _values[1] = value;
     }
-    public float TestValue
-    {
-        get => _values[2];
-        protected set => _values[2] = value;
-    }
 
-    private async void Start()
+    private void Start()
     {
-        ISkill skill = _skillData.SkillList.Find(x => x.SkillName == _skillName)
-               .SkillObj.GetComponent<ISkill>();
-
-        await skill.StartSkill();
-        PlayUpdate(skill.SkillResult());
-        skill.SkillEnd();
-    }
-
-    public void SwitchValue(int index)
-    {
-        _updating[index] = true;
-        Debug.Log($"{index + 1}番目の値を更新します");
-    }
-
-    private void PlayUpdate(float value)
-    {
-        for (int i = 0; i < _updating.Length; i++)
+        for (int i = 0; i < _values.Count; i++)
         {
-            if (_updating[i])
-                _values[i] += _updateValue * value;
-
-            _updating[i] = false;
+            _defaultValue.Add(0);
+            _defaultValue[i] = _values[i];
         }
-        Debug.Log($"武器の指定したステータスが{value}倍になりました");
     }
+
+    private async void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            ResetValues();
+
+            if (_type == AttackType.Normal)
+            {
+                if (FindObjectOfType<EnemyController>().TryGetComponent<IAddDamage>(out IAddDamage enemy))
+                {
+                    enemy.AddDamage(_values[0]);
+                }
+            }
+            else if (_type == AttackType.Skill)
+            {
+                ISkill skill = _skillData.SkillList.Find(x => x.SkillName == _skillName)
+                       .SkillObj.GetComponent<ISkill>();
+
+                await skill.StartSkill();
+
+                _values[0] += (int)(_updateValue * skill.SkillResult());
+                //PlayUpdate(skill.SkillResult());
+
+                if (FindObjectOfType<EnemyController>().TryGetComponent<IAddDamage>(out IAddDamage enemy))
+                {
+                    enemy.AddDamage(_values[0]);
+                }
+                skill.SkillEnd();
+            }
+        }
+    }
+
+    private void ResetValues()
+    {
+        for (int i = 0; i < _values.Count; i++)
+        {
+            _values[i] = _defaultValue[i];
+        }
+    }
+}
+
+public enum AttackType
+{
+    Normal,
+    Skill,
 }
