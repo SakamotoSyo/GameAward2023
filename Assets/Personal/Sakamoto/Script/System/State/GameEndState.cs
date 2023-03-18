9c737a83bff3d7450d64d255a3ed26d34d457698
sakamotosyo <syo10150000@gmail.com>
2023-03-19 01:58:13 +0900

    [Add]Stateの土台を色々と作成

---
+++ Assets/Personal/Sakamoto/Script/System/State/GameEndState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State = StateMachine<BattleStateController>.State;
+
+public class GameEndState : State
+{
+    protected override void OnEnter(State currentState)
+    {
+        
+    }
+}
---
+++ Assets/Personal/Sakamoto/Script/System/State/GameStartState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State = StateMachine<BattleStateController>.State;
+
+public class GameStartState : State
+{
+    protected override void OnEnter(State currentState)
+    {
+
+    }
+}
+
+
