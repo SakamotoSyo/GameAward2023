9dfdeea2766a0de9273195d389526f7643f1866f
tatsupiko <identity2022@outlook.jp>
2023-03-20 22:11:17 +0900

    [Fix] クリティカルが発生するたびにカメラの揺れを大きくするクラスが正常に機能するようにした。 [Apdate] 「WeaponStatus」クラスにクリティカルが発生するたびにカメラの揺れが大きくなる処理を追加した。

--- Assets/Personal/Yoshizawa/CameraShake.cs
+++ Assets/Personal/Yoshizawa/CameraShake.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using DG.Tweening;
 
 // 日本語対応
 public class CameraShake : MonoBehaviour
 {
     private CinemachineImpulseSource _cameraImpulse = null;
+    private float _defaultValue = 0f;
+
+    public bool IsReset { private get; set; } = false;
 
     private void Start()
     {
@@ -19,14 +23,24 @@ public class CameraShake : MonoBehaviour
             _cameraImpulse = gameObject.AddComponent<CinemachineImpulseSource>();
             _cameraImpulse.m_ImpulseDefinition.m_ImpulseType = CinemachineImpulseDefinition.ImpulseTypes.Uniform;
             _cameraImpulse.m_ImpulseDefinition.m_ImpulseShape = CinemachineImpulseDefinition.ImpulseShapes.Explosion;
-            _cameraImpulse.m_ImpulseDefinition.m_ImpulseDuration = 0.2f;
             _cameraImpulse.m_DefaultVelocity.y = 1f;
         }
+
+        _defaultValue = _cameraImpulse.m_DefaultVelocity.y;
     }
 
     public void CameraShakeMagnitude(float magnification)
     {
         _cameraImpulse.m_DefaultVelocity.y *= magnification;
         _cameraImpulse.GenerateImpulse();
+
+        if (IsReset)
+        {
+            DOVirtual.DelayedCall(_cameraImpulse.m_ImpulseDefinition.m_ImpulseDuration, () =>
+            {
+                _cameraImpulse.m_DefaultVelocity.y = _defaultValue;
+                IsReset = false;
+            });
+        }
     }
 }
