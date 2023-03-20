d206f85382aa7ca74adc5e6fac05eae780e2fbac
Akira Watanabe <a0323w@gmail.com>
2023-03-21 03:07:36 +0900

    [add] Animationの再生をDelayさせる

--- Assets/Personal/Watanabe/Scripts/SkillDelay.cs
+++ Assets/Personal/Watanabe/Scripts/SkillDelay.cs
@@ -5,20 +5,56 @@ public class SkillDelay : MonoBehaviour
     [Tooltip("Time.timeScaleをどれくらい下げるか")]
     [Range(0.1f, 1f)]
     [SerializeField] private float _delayScale = 0.1f;
+    [SerializeField] private float _delayTime = 1f;
+    [SerializeField] private SlowAnim _slow = default;
+
+    //private Rigidbody2D _rb2d = default;
+    private float _delaying = 0f;
+    private bool _isDelay = false;
+
+    private void Start()
+    {
+        //RigidBody.InterpolateをInterpolateにすることで、
+        //スロー中の物理演算を滑らかに行う
+        //_rb2d = GetComponent<Rigidbody2D>();
+        //_rb2d.interpolation = RigidbodyInterpolation2D.Interpolate;
+    }
+
+    private void Update()
+    {
+        //以下テスト
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!_slow.IsDelay)
+                _slow.DelayAnimation();
+        }
+
+        if (_isDelay)
+        {
+            _delaying += Time.unscaledDeltaTime;
+            if (_delaying >= _delayTime)
+            {
+                DelayReset();
+                _isDelay = false;
+            }
+        }
+    }
 
     /// <summary> スローモーション </summary>
     public void Delay()
     {
+        _delaying = 0f;
         //FixedUpdate()はTime.timeScaleの影響を受ける
         //Update()はTime.timeScaleの影響を受けない
         Time.timeScale = _delayScale;
-        Debug.LogFormat("delay");
+        _isDelay = true;
+        Debug.Log("delay");
     }
 
     /// <summary> 元に戻す </summary>
-    public void DelayReset()
+    private void DelayReset()
     {
         Time.timeScale = 1f;
-        Debug.LogFormat("delay reset");
+        Debug.Log("delay reset");
     }
 }
