aa4d81aff0378561eebca78676dc611935873ea4
RK <riku.kuu0421@gmail.com>
2023-03-23 02:30:56 +0900

    [add]EnemyHealthクラスを作成

--- Assets/Personal/Ohashi/Script/EnemyController.cs
+++ Assets/Personal/Ohashi/Script/EnemyController.cs
@@ -1,11 +1,7 @@
 ﻿using UnityEngine;
-using UniRx;
-using DG.Tweening;
 
 public class EnemyController : MonoBehaviour, IAddDamage
 {
-    [SerializeField, Tooltip("マックス時のHealth")]
-    private float _maxHealth = 5f;
 
     [SerializeField, Tooltip("ダメージテキストのクラス")]
     private DamageTextController _damegeController;
@@ -13,14 +9,10 @@ public class EnemyController : MonoBehaviour, IAddDamage
     [SerializeField, Tooltip("ダメージテキストを生成する座標")]
     private Transform _damagePos;
 
-    [SerializeField, Tooltip("敵を無敵にする")]
-    private bool _isInvincible;
+    [SerializeField]
+    private EnemyHealth _enemyHealth;
 
-    public float MaxHealth => _maxHealth;
-
-    private ReactiveProperty<float> _health = new();
-
-    public IReadOnlyReactiveProperty<float> Health => _health;
+    public EnemyHealth EnemyHealth => _enemyHealth;
 
     private SpriteRenderer _renderer;
 
@@ -28,36 +20,18 @@ public class EnemyController : MonoBehaviour, IAddDamage
 
     private void Start()
     {
-        _health.Value = _maxHealth;
         _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
-    }
-
-    /// <summary>
-    /// ダメージを受けた時のアニメーション
-    /// </summary>
-    private void DamageAnimation()
-    {
-        _animator.Play("Damage");
-        _renderer.DOColor(Color.red, 0.3f)
-            .OnComplete(() => _renderer.DOColor(Color.white, 0.3f));
+        _enemyHealth.Init(_renderer, _animator, gameObject);
     }
 
     public void AddDamage(int damage)
     {
-        if (!_isInvincible)
-        {
-            _health.Value -= damage;
-        }
-        DamageAnimation();
+        _enemyHealth.DamageAnimation();
         var damageController = Instantiate(_damegeController,
             _damagePos.position,
             Quaternion.identity);
         damageController.TextInit(damage);
-        if (_health.Value <= 0)
-        {
-            Debug.Log("Enemyの体力が0になった");
-            gameObject.SetActive(false);
-        }
+        _enemyHealth.Damage(damage);
     }
 }
