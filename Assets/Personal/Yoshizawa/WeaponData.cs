01c155ace85ee102a6a9ed00164e092da1a57e66
tatsupiko <identity2022@outlook.jp>
2023-03-29 17:51:56 +0900

    [Update] WeaponDataクラスに耐久値を管理するパラメータを追加した。

--- Assets/Personal/Yoshizawa/WeaponData.cs
+++ Assets/Personal/Yoshizawa/WeaponData.cs
@@ -4,13 +4,15 @@ public struct WeaponData
     public float OffensivePower;
     public float WeaponWeight;
     public float CriticalRate;
+    public float DurableValue;
     public AttributeType Attribute;
 
-    public WeaponData(float power, float weight, float rate, AttributeType type)
+    public WeaponData(float power, float weight, float rate, float value, AttributeType type = AttributeType.None)
     {
         OffensivePower = power;
         WeaponWeight = weight;
         CriticalRate = rate;
+        DurableValue = value;
         Attribute = type;
     }
 
@@ -19,6 +21,6 @@ public struct WeaponData
         None,
         Fire,
         Water,
-        Wood,
+        leaf,
     }
 }
