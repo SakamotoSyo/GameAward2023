fbfce7a0df9721d0f2e53a33339a5998633167d0
Akira Watanabe <a0323w@gmail.com>
2023-03-31 13:45:56 +0900

    [update] MainSceneのUI(ボタン選択)を、マウスでもできるようにした

---
+++ Assets/Personal/Watanabe/Scripts/Test/MouseButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MouseButton : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] private ButtonSelect _select = default;
+    [SerializeField] private int _index = 0;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _select.UIMoveByMouse(_index);
+    }
+}
