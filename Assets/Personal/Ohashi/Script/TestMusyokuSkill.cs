c795f7251f6c6bad8ada01961e8b88511f639f2b
RK <riku.kuu0421@gmail.com>
2023-04-02 20:54:13 +0900

    [update]攻撃するとき黒くする演出を追加

--- Assets/Personal/Ohashi/Script/TestMusyokuSkill.cs
+++ Assets/Personal/Ohashi/Script/TestMusyokuSkill.cs
@@ -10,6 +10,15 @@ public class TestMusyokuSkill : MonoBehaviour
     [SerializeField]
     private EnemyController _enemyController;
 
+    [SerializeField]
+    private GameObject _blackPlayer;
+
+    [SerializeField]
+    private GameObject _blackEnemy;
+
+    [SerializeField]
+    private SpriteRenderer _backGround;
+
     /// <summary>
     /// 無職転生のスキル
     /// </summary>
@@ -18,12 +27,28 @@ public class TestMusyokuSkill : MonoBehaviour
         _fadePanel.enabled = true;
         var sequence = DOTween.Sequence();
         //フェードアウト
-        sequence.Append(_fadePanel.DOFade(1f, 1f)
-            .OnComplete(() => _fadePanel.color = new Color(0, 0, 0, 0)));
+        sequence.Append(_fadePanel.DOFade(1f, 1f));
+        //全体の色を黒くしてフェードをやめる
+        sequence.AppendCallback(() =>
+        {
+            _backGround.color = new Color(0.1f, 0.1f, 0.1f, 1);
+            _blackPlayer.SetActive(true);
+            _blackEnemy.SetActive(true);
+            _fadePanel.color = new Color(0, 0, 0, 0);
+        });
         //プレイヤーを動かす
         sequence.Append(transform.DOMoveX(5.5f, 0.5f));
         //待つ
         sequence.AppendInterval(1f);
+        //全体の色を元に戻す
+        sequence.AppendCallback(() =>
+        {
+            _backGround.color = Color.white;
+            _blackPlayer.SetActive(false);
+            _blackEnemy.SetActive(false);
+        });
+        //待つ
+        sequence.AppendInterval(0.5f);
         //敵のダメージのメソッドを呼ぶ
         sequence.AppendCallback(() => _enemyController.AddDamage(10000));
         //待つ
