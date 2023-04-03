83eb9433483f0ac5a2f684dcc952bf6520c6f0d1
Akira Watanabe <a0323w@gmail.com>
2023-04-03 18:00:31 +0900

    [update] 選択画面のUIを見やすくした

--- Assets/Personal/Watanabe/Scripts/ClickEvent.cs
+++ Assets/Personal/Watanabe/Scripts/ClickEvent.cs
@@ -5,15 +5,15 @@ using UnityEngine.EventSystems;
 public class ClickEvent : MonoBehaviour, IPointerClickHandler
 {
     [SerializeField] private UnityEvent _onClick = default;
+    [Tooltip("ステージの情報")]
+    [Multiline(6)]
+    [SerializeField] private string _info = "";
+
+    public string Info { get => _info; protected set => _info = value;}
 
     public void OnPointerClick(PointerEventData eventData)
     {
         //オブジェクトクリック時に指定したイベントを実行する
         _onClick?.Invoke();
-    }
-
-    public void Test()
-    {
-        Debug.Log("aaa");
     }
 }
