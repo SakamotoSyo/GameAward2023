13dae8dd9b0095a0cbf99842fc9793e5f74c42e7
Akira Watanabe <a0323w@gmail.com>
2023-04-03 17:34:46 +0900

    [add] バトルステージ選択画面UI

---
+++ Assets/Personal/Watanabe/Scripts/BattleSelect.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class BattleSelect : MonoBehaviour
+{
+    [SerializeField] private Transform[] _battles = new Transform[4];
+
+    private int _index = 0;
+
+    public void MovePos(int num)
+    {
+        int n = Mathf.Abs(_index - num);
+
+        for (int i = 0; i < _battles.Length; i++)
+        {
+            if (i != num)
+            {
+                if (_index < num)
+                {
+                    _battles[i].transform.DOLocalMoveX(
+                        _battles[i].transform.localPosition.x + (-430f * n), 1f);
+                }
+                else if (_index > num)
+                {
+                    _battles[i].transform.DOLocalMoveX(
+                        _battles[i].transform.localPosition.x + (430f * n), 1f);
+                }
+            }
+            else
+            {
+                _battles[i].transform.DOLocalMoveX(-500f, 1f);
+            }
+        }
+        _index = num;
+    }
+}
---
+++ Assets/Personal/Watanabe/Scripts/ClickEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ClickEvent : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private UnityEvent _onClick = default;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        //オブジェクトクリック時に指定したイベントを実行する
+        _onClick?.Invoke();
+    }
+
+    public void Test()
+    {
+        Debug.Log("aaa");
+    }
+}
