fb140f57bdfc143c4234b943f3c30de6b07d6783
Akira Watanabe <a0323w@gmail.com>
2023-04-04 23:38:17 +0900

    [update] 各シーンのUI基礎配置

--- Assets/Personal/Watanabe/Scripts/SceneLoader.cs
+++ Assets/Personal/Watanabe/Scripts/SceneLoader.cs
@@ -1,35 +1,10 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    private readonly Dictionary<SceneName, string> _scenes = new()
-    {
-        [SceneName.TitleScene] = "Title",
-        [SceneName.HomeScene] = "Home",
-        [SceneName.RankScene] = "RankScene",
-        [SceneName.ResultScene] = "Result",
-    };
-
-    public void Load(int num)
-    {
-        var sceneName = (SceneName)num;
-
-        SceneManager.LoadScene(_scenes[sceneName]);
-    }
-
     public void Load(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
-}
-
-public enum SceneName
-{
-    //ランクシーン名は仮
-    TitleScene,
-    HomeScene,
-    RankScene,
-    ResultScene,
 }
