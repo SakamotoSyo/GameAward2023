fe8f6ed9cf3b41e7a9c6ceb71e6345b908a7b6cb
tatsupiko <identity2022@outlook.jp>
2023-04-07 01:45:12 +0900

    [Update] 「StateController」に行動順序を格納するリストとステート遷移をするときの状けんを列挙型で作成した。

--- Assets/Personal/Yoshizawa/StateController.cs
+++ Assets/Personal/Yoshizawa/StateController.cs
@@ -5,8 +5,19 @@ using UnityEngine;
 // 日本語対応
 public class StateController : MonoBehaviour
 {
+    /// <summary>戦闘時、キャラクターの行動順序を格納するリスト</summary>
+    private List<WeaponData> _orderOfAction = new List<WeaponData>();
+    public List<WeaponData> OrderOfAction => _orderOfAction;
+
     private StateMachine<StateController> _stateMachine = null;
     public StateMachine<StateController> StateMachine => _stateMachine;
+
+    private enum TransitionCondition
+    {
+        PlayerToEnemy,
+        EnemyToEnemy,
+        EnemyToPlayer,
+    }
 
     private void Awake()
     {
