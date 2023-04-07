36177ca5bbeefc8d0148304f0dffe6c4bd5ebe7e
sakamotosyo <syo10150000@gmail.com>
2023-04-07 12:40:48 +0900

    [Add]EnemyStatusを試験的に作成

---
+++ Assets/Personal/Sakamoto/Script/Actor/Enemy/EnemyStatus.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStatus
+{
+    public float Agility => _agility;
+    private float _agility;
+}
