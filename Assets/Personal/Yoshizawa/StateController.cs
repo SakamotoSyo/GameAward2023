bfd3d21a7bdb15c85e7a908ce6bd5561c8df0243
tatsupiko <identity2022@outlook.jp>
2023-04-08 15:12:26 +0900

    [Update] 「StateController」にステート遷移のTransitionを作成した。

--- Assets/Personal/Yoshizawa/StateController.cs
+++ Assets/Personal/Yoshizawa/StateController.cs
@@ -7,30 +7,37 @@ public class StateController : MonoBehaviour
 {
     /// <summary>戦闘時、キャラクターの行動順序を格納するリスト</summary>
     private List<WeaponData> _orderOfAction = new List<WeaponData>();
-    public List<WeaponData> OrderOfAction => _orderOfAction;
 
     private StateMachine<StateController> _stateMachine = null;
     public StateMachine<StateController> StateMachine => _stateMachine;
 
     private enum TransitionCondition
     {
-        PlayerToEnemy,
-        EnemyToEnemy,
-        EnemyToPlayer,
-    }
-
-    private void Awake()
-    {
-        _stateMachine = new(this);
+        Start2Player,
+        Start2Enemy,
+        Player2Enemy,
+        Enemy2Enemy,
+        Enemy2Player,
     }
 
     private void Start()
     {
-        
+        _stateMachine = new(this);
+        _stateMachine.Start<BattleStartState>();
+        _stateMachine.AddTransition<BattleStartState, PlayerStateAttack>((int)TransitionCondition.Start2Player);
+        _stateMachine.AddTransition<BattleStartState, EnemyStateAttack>((int)TransitionCondition.Start2Enemy);
+        _stateMachine.AddTransition<PlayerStateAttack, EnemyStateAttack>((int)TransitionCondition.Player2Enemy);
+        _stateMachine.AddTransition<EnemyStateAttack, EnemyStateAttack>((int)TransitionCondition.Enemy2Enemy);
+        _stateMachine.AddTransition<EnemyStateAttack, PlayerStateAttack>((int)TransitionCondition.Enemy2Player);
     }
 
     private void Update()
     {
         _stateMachine.Update();
+    }
+
+    public void CommandSelected()
+    {
+
     }
 }
