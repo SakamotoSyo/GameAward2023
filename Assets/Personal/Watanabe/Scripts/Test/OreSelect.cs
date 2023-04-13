a701b25b8c88e43332f87da558ad176a4d6439e0
Akira Watanabe <a0323w@gmail.com>
2023-04-14 02:01:47 +0900

    [update] リザルト、強化シーンUI作成中

---
+++ Assets/Personal/Watanabe/Scripts/Test/OreSelect.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class OreSelect : MonoBehaviour, IPointerClickHandler
+{
+    [Multiline(5)]
+    [SerializeField] private string _parameter = "";
+    [Multiline(3)]
+    [SerializeField] private string _flavorText = "";
+    [SerializeField] private UnityEvent _onClick = default;
+
+    public string Parameter { get => _parameter; protected set => _parameter = value; }
+    public string FlavorText { get => _flavorText; protected set => _flavorText = value; }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        _onClick?.Invoke();
+    }
+}
