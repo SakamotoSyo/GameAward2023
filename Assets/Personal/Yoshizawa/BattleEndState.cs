41fc9d32592a4bb805577f3582909966dd2bfb20
tatsupiko <identity2022@outlook.jp>
2023-04-13 23:57:48 +0900

    [Add] バトルの報酬を受け取った後にシーンを遷移させるBattleaEndStateを作成した。

---
+++ Assets/Personal/Yoshizawa/BattleEndState.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using State = StateMachine<StateController>.State;
+
+// 日本語対応
+public class BattleEndState : State
+{
+    protected override void OnEnter(State currentState)
+    {
+        Debug.Log($"{currentState}に入りました。");
+    }
+
+    protected override void OnUpdate()
+    {
+        
+    }
+
+    protected override void OnExit(State nextState)
+    {
+        Debug.Log($"{nextState}に遷移するまで、3 2 1...");
+    }
+}
