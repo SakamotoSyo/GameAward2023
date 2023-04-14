20f31bdb4be40e9875d1f68bb1e877b0c7f62640
sakamotosyo <syo10150000@gmail.com>
2023-04-14 16:10:49 +0900

    [Update]WeaponDataをStructに変更

--- Assets/Personal/Yoshizawa/WeaponData.cs
+++ Assets/Personal/Yoshizawa/WeaponData.cs
@@ -1,29 +1,36 @@
 using UniRx;
 
 // 日本語対応
-public class WeaponData
+public struct WeaponData
 {
     public float OffensivePower;
     public float WeaponWeight;
     public float CriticalRate;
     public float MaxDurable;
     public float CurrentDurable;
+    public float[] StatusArray;
     public AttributeType Attribute;
     public WeaponType WeaponType;
-    
+
 
     /// <summary>武器のパラメータを格納する構造体 | 属性は初期設定だと無属性</summary>
-    //public WeaponData(float offensivePower, float weaponWeight,
-    //    float criticalRate, float durableValue, AttributeType attributeType = AttributeType.None, WeaponType weponType = WeaponType.GreatSword)
-    //{
-    //    OffensivePower = offensivePower;
-    //    WeaponWeight = weaponWeight;
-    //    CriticalRate = criticalRate;
-    //    MaxDurable = durableValue;
-    //    CurrentDurable = durableValue;
-    //    Attribute = attributeType;
-    //    WeaponType = weponType;
-    //}
+    public WeaponData(float offensivePower, float weaponWeight,
+        float criticalRate, float durableValue, AttributeType attributeType = AttributeType.None, WeaponType weponType = WeaponType.GreatSword)
+    {
+        OffensivePower = offensivePower;
+        WeaponWeight = weaponWeight;
+        CriticalRate = criticalRate;
+        MaxDurable = durableValue;
+        CurrentDurable = durableValue;
+        Attribute = attributeType;
+        WeaponType = weponType;
+
+        StatusArray = new float[4];
+        StatusArray[0] = offensivePower;
+        StatusArray[1] = weaponWeight;
+        StatusArray[2] = criticalRate;
+        StatusArray[3] = durableValue;
+    }
 
     public void UpdateParam(PlayerEquipWeapon equipWeaponData) 
     {
@@ -34,6 +41,20 @@ public class WeaponData
         CurrentDurable = equipWeaponData.CurrentDurable.Value;
         Attribute = equipWeaponData.Attribute;
         WeaponType = equipWeaponData.WeaponType;
+    }
+
+    /// <summary>
+    /// Statusを配列にして返す
+    /// </summary>
+    /// <returns></returns>
+    public float[] GetStatusArray() 
+    {
+        float[] statusArray = new float[4];
+        statusArray[0] = OffensivePower;
+        statusArray[1] = WeaponWeight;
+        statusArray[2] = CriticalRate;
+        statusArray[3] = MaxDurable;
+        return statusArray;
     }
     
     public enum AttributeType
