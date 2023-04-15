0cd6b29a6fa915c19b06047d0f574efd60200a8d
RK <riku.kuu0421@gmail.com>
2023-04-15 23:55:24 +0900

    [add]enemyDateのスクリプタブルオブジェクトを作成

---
+++ Assets/Personal/Ohashi/Script/Enemy/EnemyDate.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class EnemyDate : ScriptableObject
+{
+    public Sprite EnemySprite;
+
+    public Animator EnemyAnim;
+
+    public WeaponData[] WeaponDates;
+
+}
