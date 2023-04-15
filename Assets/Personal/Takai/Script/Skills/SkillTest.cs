f4db37ac7ea914cdc3acb7fc7890ea23053c7b9a
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-04-15 18:54:38 +0900

    [add]ResourcesファイルにSkillsファイル作成・クラス各箇所変更

--- Assets/Personal/Takai/Script/Skills/SkillTest.cs
+++ Assets/Personal/Takai/Script/Skills/SkillTest.cs
@@ -4,49 +4,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cysharp.Threading.Tasks;
 
-public class SkillTest : MonoBehaviour, ISkillBase 
+public  class SkillTest : SkillBase 
 {
     public string SkillName { get; set; }
     public int Damage { get; set; }
     public WeaponType Weapon { get; set; }
     public OreRarity Rarity { get; set; }
     public SkillType Type  { get; set; }
-    public PlayerSkillDataManagement playerSkillDataManagement { get; set; }
-
+    
     private Animator _anim;
-    private void Start()
-    {
-        SendSkill();
-    }
 
-    public void SetUp()
-    {
-        SkillName = "TestSkill";
-        Damage = 5;
-        Weapon = WeaponType.GreatSword;
-        Rarity = OreRarity.Normal;
-        Type = SkillType.Skill;
-        _anim = GetComponent<Animator>();
-    }
-
-    public void SendSkill()
-    {
-        playerSkillDataManagement = FindObjectOfType<PlayerSkillDataManagement>();
-
-        //SkillTest mySkill = new SkillTest();
-        SetUp();
-
-        playerSkillDataManagement.AddSkill(this);
-    }
-
-    public async UniTask UseSkill()
+    public override async UniTask UseSkill()
     {
         Debug.Log("Use Skill");
+        _anim = GetComponent<Animator>();
         await UniTask.WaitUntil(() => _anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
         Debug.Log("Anim End");
     }
 
-    public void SkillEffect()
+    protected override void SkillEffect()
     {
         Debug.Log("Skill Effect");
     }
