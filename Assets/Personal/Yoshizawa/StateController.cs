2ee79f29b5cf9f822a81c3562d60b34266f3915a
tatsupiko <identity2022@outlook.jp>
2023-04-16 23:13:24 +0900

    [Update] 「StateController」に戦闘継続か戦闘終了かの判定をする関数を追加した。

--- Assets/Personal/Yoshizawa/StateController.cs
+++ Assets/Personal/Yoshizawa/StateController.cs
@@ -19,6 +19,16 @@ public class StateController : MonoBehaviour
     private PlayerController _playerController = null;
     public PlayerController PlayerController => _playerController;
 
+    private ResultUIScript _resultUI = null;
+    public ResultUIScript ResultUI => _resultUI;
+
+    [SerializeField]
+    private string _titleScene = "";
+    public string TitleScene => _titleScene;
+    [SerializeField]
+    private string _homeScene = "";
+    public string HomeScene => _homeScene;
+
     public enum TransitionCondition
     {
         Any2Start,
@@ -41,6 +51,8 @@ public class StateController : MonoBehaviour
         {
             _orderOfAction.Add(_enemyController[i].gameObject);
         }
+
+        _resultUI = FindObjectOfType<ResultUIScript>();
         
         _stateMachine = new(this);
         _stateMachine.AddAnyTranstion<BattleStartState>((int)TransitionCondition.Any2Start);
@@ -59,23 +71,18 @@ public class StateController : MonoBehaviour
     {
         if (BattleStartState.IsBattleCommandSelected)
         {
+            // リストを所持武器の重さで並べ替えている
             _orderOfAction.Sort((a, b) =>
             {
                 float x = 0f, y = 0f;
                 
                 // 変数xに代入する値の選定
-                if (a.TryGetComponent(out EnemyController enemyA)) x = enemyA.EnemyStatus.GetSpeed;
-                else if (a.TryGetComponent(out PlayerController player))
-                {
-                    x = player.PlayerStatus.EquipWeapon.WeaponWeight.Value;
-                }
+                if (a.TryGetComponent(out EnemyController enemyA)) x = enemyA.EnemyStatus.EquipEnemyWeapon.WeaponWeight;
+                else if (a.TryGetComponent(out PlayerController player)) x = player.PlayerStatus.EquipWeapon.WeaponWeight.Value;
 
                 // 変数yに代入する値の選定
-                if (b.TryGetComponent(out EnemyController enemyB)) y = enemyB.EnemyStatus.GetSpeed;
-                else if (b.TryGetComponent(out PlayerController player))
-                {
-                    y = player.PlayerStatus.EquipWeapon.WeaponWeight.Value;
-                }
+                if (b.TryGetComponent(out EnemyController enemyB)) y = enemyB.EnemyStatus.EquipEnemyWeapon.WeaponWeight;
+                else if (b.TryGetComponent(out PlayerController player)) y = player.PlayerStatus.EquipWeapon.WeaponWeight.Value;
 
                 if (x - y > 0)      return -1;
                 else if (x - y < 0) return  1;
@@ -107,36 +114,61 @@ public class StateController : MonoBehaviour
     {
         GameObject currentActor = _orderOfAction[_count];
 
-        if (currentActor.TryGetComponent(out EnemyController enemy))
-        {
-            CurrenyEnemy = enemy;
-        }
-
         if (_count < _orderOfAction.Count) _count++;
         else
         {
             _count = 0;
-            _stateMachine.Dispatch((int)TransitionCondition.Any2Start);
+            TransitionToStartOrEnd(CurrenyEnemy.EnemyStatus.WeaponDates);
         }
 
         GameObject nextActor = _orderOfAction[_count];
 
-        // 次の演者
+        // 次に行動するActorがEnemyだったら
         if (nextActor.TryGetComponent(out EnemyController nextEnemy))
         {
+            // 現在行動しているActorがEnemyだったら
             if (currentActor.TryGetComponent(out EnemyController currentEnemy))
             {
                 _stateMachine.Dispatch((int)TransitionCondition.Enemy2Enemy);
             }
+            // 現在行動しているActorがPlayerだったら
             else if (currentActor.TryGetComponent(out PlayerController currentPlayer))
             {
+                CurrenyEnemy = nextEnemy;
                 _stateMachine.Dispatch((int)TransitionCondition.Player2Enemy);
             }
         }
-        // 
+        // 次に行動するActorがPlayerだったら
         else if (nextActor.TryGetComponent(out PlayerController nextPlayer))
         {
             _stateMachine.Dispatch((int)TransitionCondition.Enemy2Player);
         }
+    }
+
+    /// <summary>戦闘開始 or 戦闘終了 のステートに遷移する</summary>
+    /// <param name="weapons">所持武器の配列</param>
+    public void TransitionToStartOrEnd(WeaponData[] weapons)
+    {
+        if (BrokenWeaponsCount(weapons) < weapons.Length)
+        {
+            _stateMachine.Dispatch((int)TransitionCondition.Any2Start);
+        }
+        else
+        {
+            _stateMachine.Dispatch((int)TransitionCondition.Any2End);
+        }
+    }
+
+    /// <summary>壊れた武器の数を返す</summary>
+    public int BrokenWeaponsCount(WeaponData[] weapons)
+    {
+        int count = 0;
+
+        foreach (var weapon in weapons)
+        {
+            if (weapon.CurrentDurable <= 0f) count++;
+        }
+
+        return count;
     }
 }
