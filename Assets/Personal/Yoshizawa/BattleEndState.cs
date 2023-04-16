9d1cd8cf72e2febd034e564504ef9622d0fc0ac6
tatsupiko <identity2022@outlook.jp>
2023-04-16 23:10:45 +0900

    [Update] 「BattleEndState」にゲームクリア処理とゲームオーバー処理を追加した。

--- Assets/Personal/Yoshizawa/BattleEndState.cs
+++ Assets/Personal/Yoshizawa/BattleEndState.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using State = StateMachine<StateController>.State;
 
 // 日本語対応
@@ -9,5 +10,18 @@ public class BattleEndState : State
     protected override void OnEnter(State currentState)
     {
         Debug.Log($"{currentState}に入りました。");
+        WeaponData[] playerWeapons = Owner.PlayerController.PlayerStatus.WeaponDatas;
+
+        // ゲームクリア判定
+        if (Owner.BrokenWeaponsCount(playerWeapons) < playerWeapons.Length)
+        {
+            Owner.ResultUI.StartResultLottery();
+            SceneManager.LoadScene(Owner.HomeScene);
+        }
+        // ゲームオーバー判定
+        else
+        {
+            SceneManager.LoadScene(Owner.TitleScene);
+        }
     }
 }
