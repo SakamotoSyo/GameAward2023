18a23d745dd3fbe66e464da5fbf03d891f1e6563
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-04-17 21:23:24 +0900

    [update]SkillGenerator.cs改良

--- Assets/Personal/Takai/Editor/SkillGenerator.cs
+++ Assets/Personal/Takai/Editor/SkillGenerator.cs
@@ -1,18 +1,98 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using UnityEditor;
 using UnityEngine;
+using System.IO;
+using UnityEngine.SceneManagement;
 
-public class SkillGenerator : MonoBehaviour
+[CustomEditor(typeof(SkillBase))]
+public class SkillGenerator : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    private string _skillName = "";
+    private int _damage = 0;
+    private WeaponType _weapon;
+    private OreRarity _rarity;
+    private SkillType _type;
+    private PlayerSkillDataManagement _playerSkillDataManagement;
+
+    private string _className = "";
+
+    [MenuItem("Window/SkillGenerator")]
+    static void Open()
     {
-        
+        var window = GetWindow<SkillGenerator>();
+        window.titleContent = new GUIContent("SkillGenerator");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        GUILayout.Label("スキルステータス設定");
+
+        EditorGUILayout.BeginVertical(GUI.skin.box);
+        _skillName = EditorGUILayout.TextField("スキル名", _skillName);
+        _damage = EditorGUILayout.IntField("ダメージ", _damage);
+        _weapon = (WeaponType)EditorGUILayout.EnumPopup("武器種類", _weapon);
+        _rarity = (OreRarity)EditorGUILayout.EnumPopup("レアリティ", _rarity);
+        _type = (SkillType)EditorGUILayout.EnumPopup("タイプ", _type);
+        _className = EditorGUILayout.TextField("クラス名", _className);
+        if (GUILayout.Button("リセット"))
+        {
+            Reset();
+        }
+
+        if (GUILayout.Button("作成"))
+        {
+            CreatSkill();
+        }
+
+        EditorGUILayout.EndVertical();
+    }
+
+    private void Reset()
+    {
+        _skillName = "";
+        _damage = 0;
+        _skillName = "";
+    }
+
+    private void CreatSkill()
+    {
+        if (_skillName == "" || _skillName == "") { return; }
+        string prefabPath = $"Assets/Resources/Skills/{_className}.prefab";
+        GameObject newPrefab = new GameObject(_className);
+
+        CreateClass();
+        while(EditorApplication.isCompiling)
+        {
+            System.Threading.Thread.Sleep(100);
+        }
+
+        newPrefab.AddComponent(Type.GetType(_className));
+        PrefabUtility.SaveAsPrefabAsset(newPrefab, prefabPath);
+        GameObject.DestroyImmediate(newPrefab);
+        AssetDatabase.Refresh();
+    }
+
+    private void CreateClass()
+    {
+        string path = "";
+
+        switch (_weapon)
+        {
+            case WeaponType.GreatSword:
+                path = $"Assets/Personal/Takai/Script/Skills/GreatSword/{_className}.cs";
+                break;
+            case WeaponType.DualBlades:
+                path = $"Assets/Personal/Takai/Script/Skills/DualBlades/{_className}.cs";
+                break;
+            case WeaponType.Hammer:
+                path = $"Assets/Personal/Takai/Script/Skills/Hammer/{_className}.cs";
+                break;
+            case WeaponType.Spear:
+                path = $"Assets/Personal/Takai/Script/Skills/Spear/{_className}.cs";
+                break;
+        }
+
+        string classCode = "using System;\r\nusing System.Collections;\r\nusing System.Collections.Generic;\r\nusing UnityEngine;\r\nusing Cysharp.Threading.Tasks;\r\nusing UnityEngine.Playables;\r\n\r\npublic  class " + _className + " : SkillBase \r\n{\r\n    public string SkillName { get; set; }\r\n    public int Damage { get; set; }\r\n    public WeaponType Weapon { get; set; }\r\n    public OreRarity Rarity { get; set; }\r\n    public SkillType Type  { get; set; }\r\n    \r\n    private PlayableDirector _anim;\r\n\r\n    public override async UniTask UseSkill()\r\n    {\r\n        Debug.Log(\"Use Skill\");\r\n        _anim = GetComponent<PlayableDirector>();\r\n        await UniTask.WaitUntil(() => _anim.state == PlayState.Paused);\r\n        Debug.Log(\"Anim End\");\r\n    }\r\n\r\n    protected override void SkillEffect()\r\n    {\r\n        Debug.Log(\"Skill Effect\");\r\n    }\r\n}";
+        File.WriteAllText(path, classCode);
     }
 }
