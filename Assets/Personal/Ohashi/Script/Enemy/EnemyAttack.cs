f0839b4ffec3f1995e00ba94234e4a4e95df4258
RK <riku.kuu0421@gmail.com>
2023-04-19 17:58:09 +0900

    [update]アニメーションを待つ処理を追加

--- Assets/Personal/Ohashi/Script/Enemy/EnemyAttack.cs
+++ Assets/Personal/Ohashi/Script/Enemy/EnemyAttack.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public class EnemyAttack
 {
     private float _offensivePower;
+    private Animator _anim;
 
-    public void Init(float offensivePower)
+    public void Init(float offensivePower, Animator anim)
     {
         _offensivePower = offensivePower;
+        _anim = anim;
     }
 
     /// <summary>
     /// çUåÇ
     /// </summary>
-    public void NormalAttack(PlayerController playerController)
+    public async UniTask NormalAttack(PlayerController playerController)
     {
         playerController.AddDamage(_offensivePower);
+        await UniTask.WaitUntil(() => _anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
     }
 }
