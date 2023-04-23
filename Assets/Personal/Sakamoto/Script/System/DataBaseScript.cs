2c7d278c3ee5a76ad83122536a0ec919623d9086
sakamotosyo <syo10150000@gmail.com>
2023-04-23 23:41:00 +0900

    [Add]初回準備シーンを作成

--- Assets/Personal/Sakamoto/Script/System/DataBaseScript.cs
+++ Assets/Personal/Sakamoto/Script/System/DataBaseScript.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 using UnityEngine.UI;
 using System.IO;
 
 public class DataBaseScript : MonoBehaviour
 {
     public static List<EnhanceData> DescriptionEnhanceData => _enhanceData;
+    public static Dictionary<WeaponType, string> WeaponDescriptionData => _weaponDescription;
 
     [Header("レベルアップデータ")]
     [SerializeField] private TextAsset _levelUpTable;
     [SerializeField] private TextAsset _descriptionData;
+    [SerializeField] private TextAsset _weaponDescriptionData;
+    private static Dictionary<WeaponType, string> _weaponDescription = new Dictionary<WeaponType, string>();
     private static Dictionary<int, int> _statusData = new Dictionary<int, int>();
     private static List<EnhanceData> _enhanceData = new();
 
@@ -21,7 +25,7 @@ public class DataBaseScript : MonoBehaviour
         {
             SetDescription();
         }
-        
+
     }
 
     /// <summary>
@@ -52,7 +56,7 @@ public class DataBaseScript : MonoBehaviour
         }
     }
 
-    private void SetDescription() 
+    private void SetDescription()
     {
         StringReader sr = new StringReader(_descriptionData.text);
         //最初の一行目はスキップ
@@ -72,12 +76,47 @@ public class DataBaseScript : MonoBehaviour
         }
     }
 
+    private void SetWeaponTypeDescription()
+    {
+        //テキストの読み込み
+        StringReader sr = new StringReader(_weaponDescriptionData.text);
+        //最初の一行目はスキップ
+        sr.ReadLine();
+
+        while (true)
+        {
+            //一行ずつ読み込む
+            string line = sr.ReadLine();
+
+            if (string.IsNullOrEmpty(line))
+            {
+                break;
+            }
+
+            string[] parts = line.Split(',');
+            var weaponType = Enum.Parse<WeaponType>(parts[0]);
+            string description = "";
+
+            for (int i = 1; i < parts.Length; i++)
+            {
+                if (i != 1)
+                {
+                    description += "\n";
+                }
+
+                description += parts[i];
+            }
+            _weaponDescription.Add(weaponType, description);
+        }
+
+    }
+
     /// <summary>
     /// レベルに対しての次のレベルアップまでの経験値を返してくれる
     /// </summary>
     /// <param name="Level"></param>
     /// <returns></returns>
-    public static int GetMaxExperienceAmount(int Level) 
+    public static int GetMaxExperienceAmount(int Level)
     {
         return _statusData[Level];
     }
---
+++ Assets/Personal/Sakamoto/Script/System/IntialPreparationScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class IntialPreparationScript : MonoBehaviour
+{
+    public List<WeaponData> WeaponDatas => _weaponData;
+    private List<WeaponData> _weaponData = new();
+
+    /// <summary>
+    /// 武器を配列にいれれることが出来るか確認して入れる
+    /// </summary>
+    /// <param name="weaponType"></param>
+    /// <returns></returns>
+    public bool SetWeaponTypeConfirmation(WeaponData weaponType)
+    {
+        WeaponData[] array = new WeaponData[2];
+        if (_weaponData.Count != 0)
+        {
+            array = _weaponData.Where(x => x.WeaponType == weaponType.WeaponType).ToArray();
+        }
+        else 
+        {
+            _weaponData.Add(weaponType);
+            return true;
+        }
+        
+        if (array.Length == 0 && _weaponData.Count < 2)
+        {
+            _weaponData.Add(weaponType);
+            return true;
+        }
+        else 
+        {
+           return false;
+        }
+    }
+}
