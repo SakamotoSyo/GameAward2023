2b4a63da740f64aad7de20cfd010625291763842
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-04-24 02:11:51 +0900

    [add]ShishifunjinSkill作成

---
+++ Assets/Personal/Takai/Script/Skills/Spear/ShishifunjinSkill.cs
@@ -0,0 +1,35 @@
+
+    using UnityEngine;
+    using Cysharp.Threading.Tasks;
+    using UnityEngine.Playables;
+
+    public class ShishifunjinSkill : SkillBase
+    {
+        public override string SkillName { get; protected set; }
+        public override int Damage { get; protected set; }
+        public override WeaponType Weapon { get; protected set; }
+        public override SkillType Type { get; protected set; }
+        private PlayableDirector _anim;
+
+    public ShishifunjinSkill()
+    {
+        SkillName = "獅子奮迅";
+        Damage = 20;
+        Weapon = (WeaponType)3;
+        Type = (SkillType)0;
+    }
+
+        public async override UniTask UseSkill(PlayerStatus status)
+        {
+            Debug.Log("Use Skill");
+            _anim = GetComponent<PlayableDirector>();
+            SkillEffect(status);
+            await UniTask.WaitUntil(() => _anim.state == PlayState.Paused);
+            Debug.Log("Anim End");
+        }
+
+        protected override void SkillEffect(PlayerStatus status)
+        {
+            // スキルの効果処理を実装する
+        }
+    }
