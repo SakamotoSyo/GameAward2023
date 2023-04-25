c509c7405c9b3d57c93a9a6972a166d377d15110
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-04-25 13:50:20 +0900

    [add]IatsuSkill作成

---
+++ Assets/Personal/Takai/Script/Skills/GreatSword/IatsuSKill.cs
@@ -0,0 +1,36 @@
+
+    using UnityEngine;
+    using Cysharp.Threading.Tasks;
+    using UnityEngine.Playables;
+
+    public class IatsuSKill : SkillBase
+    {
+        public override string SkillName { get; protected set; }
+        public override int Damage { get; protected set; }
+        public override WeaponType Weapon { get; protected set; }
+        public override SkillType Type { get; protected set; }
+        public override string FlavorText { get; protected set; }
+        private PlayableDirector _anim;
+
+    public IatsuSKill()
+    {
+        SkillName = "威圧";
+        Damage = 0;
+        Weapon = (WeaponType)0;
+        Type = (SkillType)0;
+    }
+
+        public async override UniTask UseSkill(PlayerStatus status)
+        {
+            Debug.Log("Use Skill");
+            _anim = GetComponent<PlayableDirector>();
+            SkillEffect(status);
+            await UniTask.WaitUntil(() => _anim.state == PlayState.Paused);
+            Debug.Log("Anim End");
+        }
+
+        protected override void SkillEffect(PlayerStatus status)
+        {
+            // スキルの効果処理を実装する
+        }
+    }
