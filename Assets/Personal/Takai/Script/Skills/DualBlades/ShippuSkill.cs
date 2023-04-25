a88e2f31f78956ef5ab6466fbb6f21a1188412fd
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-04-25 13:50:05 +0900

    [add]ShippuSkill作成

---
+++ Assets/Personal/Takai/Script/Skills/DualBlades/ShippuSkill.cs
@@ -0,0 +1,36 @@
+
+    using UnityEngine;
+    using Cysharp.Threading.Tasks;
+    using UnityEngine.Playables;
+
+    public class ShippuSkill : SkillBase
+    {
+        public override string SkillName { get; protected set; }
+        public override int Damage { get; protected set; }
+        public override WeaponType Weapon { get; protected set; }
+        public override SkillType Type { get; protected set; }
+        public override string FlavorText { get; protected set; }
+        private PlayableDirector _anim;
+
+    public ShippuSkill()
+    {
+        SkillName = "疾風";
+        Damage = 30;
+        Weapon = (WeaponType)1;
+        Type = (SkillType)0;
+    }
+
+        public async override UniTask UseSkill(PlayerStatus status)
+        {
+            Debug.Log("Use Skill");
+            _anim = GetComponent<PlayableDirector>();
+            SkillEffect(status);
+            await UniTask.WaitUntil(() => _anim.state == PlayState.Paused);
+            Debug.Log("Anim End");
+        }
+
+        protected override void SkillEffect(PlayerStatus status)
+        {
+            // スキルの効果処理を実装する
+        }
+    }
