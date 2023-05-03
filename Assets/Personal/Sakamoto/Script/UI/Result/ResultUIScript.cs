e2e49fc2619795deb46466ba626fe92218fdc3f6
sakamotosyo <syo10150000@gmail.com>
2023-05-03 20:13:42 +0900

    [Fix]変数の名前を変更

---
+++ Assets/Personal/Sakamoto/Script/UI/Result/ResultUIScript.cs
@@ -0,0 +1,248 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using Random = UnityEngine.Random;
+
+public class ResultUIScript : MonoBehaviour
+{
+    [SerializeField] private OreUIScript[] _oreUiCs = new OreUIScript[3];
+    [SerializeField] private Button[] _oreButton = new Button[3];
+    [SerializeField] private Button[] _weaponButton = new Button[4];
+    [SerializeField] private Button[] _skillSelectButton = new Button[2];
+    [SerializeField] private Button[] _blackSmithSelectButton = new Button[2];
+    [SerializeField] private GameObject _oreSelectObj;
+    [SerializeField] private GameObject _enhanceSelectObj;
+    [SerializeField] private GameObject _skillSelectPanel;
+    [SerializeField] private GameObject _blacksmithSelectPanel;
+    [SerializeField] private SkillSelectButtonScript[] _skillSelectButtonCs = new SkillSelectButtonScript[2];
+    [SerializeField] private ActorGenerator _actorGenerator;
+    [SerializeField] private SkillDataManagement _playerSkillDataManagement;
+    [SerializeField] private Image[] _oreImage = new Image[3];
+    [SerializeField] private ResultWeaponButton[] _resultUICs = new ResultWeaponButton[4];
+    [SceneName]
+    [SerializeField] private string _blacksmithSceneName;
+    [Tooltip("リザルトシーンのテスト用")]
+    [SerializeField] private bool _resultSceneTest = false;
+    [SceneName]
+    [SerializeField] private string _homeScene;
+    private bool _isBlacksmith = false;
+
+
+    void Start()
+    {
+        if (_resultSceneTest)
+        {
+            StartResultLottery();
+        }
+
+        if (_actorGenerator.EnemyController.EnemyStatus.IsBoss)
+        {
+            FindWeaponTypeDontHave();
+        }
+    }
+
+    void Update()
+    {
+
+    }
+    public void StartResultLottery()
+    {
+        _oreSelectObj.SetActive(true);
+        RewardLottery();
+    }
+
+    /// <summary>
+    /// 報酬の抽選をしてデータを渡す
+    /// </summary>
+    private void RewardLottery()
+    {
+        for (int i = 0; i < _oreUiCs.Length; i++)
+        {
+            var oreInfo = RarityLottery();
+            Debug.Log(oreInfo.randomSkill);
+            OreData Ore = new OreData(SetEnhanceData(), oreInfo.rearity, oreInfo.randomSkill, oreInfo.oreImage);
+            _oreUiCs[i].SetData(Ore);
+            _oreButton[i].onClick.AddListener(() => EnhanceWeaponEvent(Ore));
+        }
+    }
+
+    /// <summary>
+    /// 強化するStatusを返してくれる
+    /// </summary>
+    /// <returns></returns>
+    private EnhanceData[] SetEnhanceData()
+    {
+        EnhanceData[] enhanceData = new EnhanceData[DataBaseScript.DescriptionEnhanceData.Count];
+        for (int i = 0; i < DataBaseScript.DescriptionEnhanceData.Count; i++)
+        {
+            //そのステータスを強化するかどうか
+            int enhanceBool = Random.Range(0, 2);
+            if (enhanceBool == 0)
+            {
+                //強化する数字
+                int enhanceNum = Random.Range(1, 3);
+                enhanceData[i].EnhanceNum = enhanceNum;
+                enhanceData[i].EnhanceDescription = DataBaseScript.DescriptionEnhanceData[i].EnhanceDescription;
+            }
+        }
+        return enhanceData;
+    }
+
+    private (Image oreImage, OreRarity rearity, SkillBase randomSkill) RarityLottery()
+    {
+        OreRarity rearity = (OreRarity)Random.Range(0, 3);
+        //WeaponData[] weaponDatas = _actorGenerator.PlayerController.PlayerStatus.WeponDatas;
+        //WeaponData weaponData = weaponDatas[Random.Range(0, weaponDatas.Length)];
+        //TODO:テストで動かせるようになっているのでPlayerが生成される環境だったら上を使う
+        //WeaponType weaponType = (WeaponType)Random.Range(0, 4);
+        SkillBase randomSkill = _playerSkillDataManagement.OnSkillCall(WeaponType.GreatSword, SkillType.Skill);
+        Image oreImage = _oreImage[(int)rearity];
+
+        return (oreImage, rearity, randomSkill);
+    }
+
+    /// <summary>
+    ///鉱石を選んだ際にどの武器に鉱石を混ぜるか選択できるEventに移る
+    /// </summary>
+    public void EnhanceWeaponEvent(OreData selectOreData)
+    {
+        _oreSelectObj.SetActive(false);
+        _enhanceSelectObj.SetActive(true);
+        WeaponData[] weaponDatas;
+
+        if (_resultSceneTest)
+        {
+            weaponDatas = new WeaponData[4];
+            for (int i = 0; i < weaponDatas.Length; i++)
+            {
+                weaponDatas[i] = new(50, 50, 50, 50, WeaponData.AttributeType.None, WeaponType.GreatSword);
+            }
+        }
+        else
+        {
+            weaponDatas = _actorGenerator.PlayerController.PlayerStatus.WeaponDatas;
+        }
+
+        for (int i = 0; i < _weaponButton.Length; i++)
+        {
+            if (weaponDatas[i] != null)
+            {
+                _weaponButton[i].enabled = true;
+                _resultUICs[i].SetUpInfo(weaponDatas[i], selectOreData);
+                var num = i;
+                _weaponButton[i].onClick.AddListener(() => WeaponEnhanceEvent(weaponDatas[num], selectOreData));
+            }
+            else
+            {
+                _weaponButton[i].enabled = false;
+            }
+
+        }
+
+    }
+
+
+    /// <summary>
+    /// 武器の強化したときのButtonEvent
+    /// </summary>
+    public void WeaponEnhanceEvent(WeaponData weaponData, OreData oreData)
+    {
+        weaponData.EnhanceParam(oreData.EnhancedData);
+        var playerSkill = _actorGenerator.PlayerController.PlayerSkill;
+        Debug.Log("強化しました");
+        if (oreData.Skill != null)
+        {
+            if (playerSkill.AddSkillJudge(oreData.Skill))
+            {
+                //スキルを追加出来たときの処理
+                Debug.Log($"{oreData.Skill}を覚えました");
+                if (_isBlacksmith)
+                {
+                    BlacksmithJudge();
+                }
+                else
+                {
+                    _actorGenerator.PlayerController.SavePlayerData();
+                    SceneLoader.LoadScene(_homeScene);
+                }
+
+            }
+            else
+            {
+                //スキルが追加できなかったときPlayerに選択させる
+                _enhanceSelectObj.SetActive(false);
+                _skillSelectPanel.SetActive(true);
+                _skillSelectButtonCs[0].SetCurrentSkill(playerSkill.PlayerSkillArray[0]);
+                _skillSelectButtonCs[1].SetCurrentSkill(playerSkill.PlayerSkillArray[1]);
+                _skillSelectButton[0].onClick.AddListener(() => ChangeSkill(0, oreData.Skill));
+                _skillSelectButton[1].onClick.AddListener(() => ChangeSkill(1, oreData.Skill));
+            }
+        }
+        // SceneLoader.LoadScene(_homeScene);
+    }
+
+    /// <summary>
+    /// 倒した敵がボスだったら武器をゲットする
+    /// </summary>
+    public void BlacksmithJudge()
+    {
+        _enhanceSelectObj.SetActive(false);
+        _skillSelectPanel.SetActive(false);
+        _blacksmithSelectPanel.SetActive(true);
+        _actorGenerator.PlayerController.SavePlayerData();
+        _blackSmithSelectButton[0].onClick.AddListener(() => SceneLoader.LoadScene(_blacksmithSceneName));
+        _blackSmithSelectButton[1].onClick.AddListener(() => SceneLoader.LoadScene(_homeScene));
+    }
+
+    /// <summary>
+    /// 持っていない武器を探して次のシーンへ移る
+    /// </summary>
+    public void FindWeaponTypeDontHave()
+    {
+        var playerStatus = _actorGenerator.PlayerController.PlayerStatus;
+
+        for (int i = 0; i < Enum.GetValues(typeof(WeaponType)).Length; i++)
+        {
+            var judge = false;
+            WeaponType weaponType = (WeaponType)Enum.GetValues(typeof(WeaponType)).GetValue(i);
+            for (int j = 0; j < playerStatus.WeaponDatas.Length; j++)
+            {
+                if (playerStatus.WeaponDatas[j].WeaponType == weaponType)
+                {
+                    judge = true;
+                }
+            }
+
+            if (!judge)
+            {
+                _isBlacksmith = true;
+                GameManager.SetBlacksmithType((WeaponType)Enum.GetValues(typeof(WeaponType)).GetValue(i));
+                //SceneLoader.LoadScene(_blacksmithSceneName);
+            }
+        }
+    }
+
+    public void ChangeSkill(int num, SkillBase skill)
+    {
+        _actorGenerator.PlayerController.PlayerSkill.PlayerSkillArray[num] = skill;
+        Debug.Log($"{skill}に変更しました");
+
+        if (_isBlacksmith)
+        {
+            BlacksmithJudge();
+        }
+        else
+        {
+            _actorGenerator.PlayerController.SavePlayerData();
+            SceneLoader.LoadScene(_homeScene);
+        }
+    }
+
+    public void SelectAgain()
+    {
+        _oreSelectObj.SetActive(true);
+        _enhanceSelectObj.SetActive(false);
+    }
+}
