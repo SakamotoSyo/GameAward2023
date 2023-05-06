20cc720f5262e178410db43b83f50d8fb52d17f8
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-05-06 19:42:24 +0900

    [update]激烈・砕き打ちスキル作成

--- Assets/Personal/Takai/Script/Skills/Hammer/GekiretsuKudakiuchiSkill.cs
+++ Assets/Personal/Takai/Script/Skills/Hammer/GekiretsuKudakiuchiSkill.cs
@@ -12,7 +12,9 @@ public class GekiretsuKudakiuchiSkill : SkillBase
     private PlayableDirector _anim;
     private PlayerStatus _playerStatus;
     private EnemyStatus _enemyStatus;
+    private ActorAttackType _actor;
     const float _subtractValue = 0.4f;
+    private bool _isUse;
 
     public GekiretsuKudakiuchiSkill()
     {
@@ -22,37 +24,83 @@ public class GekiretsuKudakiuchiSkill : SkillBase
         Type = (SkillType)1;
     }
 
-    public async override UniTask UseSkill(PlayerStatus player, EnemyStatus enemy, WeaponStatus weapon, ActorAttackType actorType)
+    public async override UniTask UseSkill(PlayerStatus player, EnemyStatus enemy, WeaponStatus weapon,
+        ActorAttackType actorType)
     {
         Debug.Log("Use Skill");
         _playerStatus = player;
         _enemyStatus = enemy;
+        _actor = actorType;
         _anim = GetComponent<PlayableDirector>();
-        SkillEffect(); 
-        await UniTask.WaitUntil(() => _anim.state == PlayState.Paused, cancellationToken: this.GetCancellationTokenOnDestroy());
+        SkillEffect();
+        await UniTask.WaitUntil(() => _anim.state == PlayState.Paused,
+            cancellationToken: this.GetCancellationTokenOnDestroy());
         Debug.Log("Anim End");
     }
 
     protected override void SkillEffect()
     {
-        // スキルの効果処理を実装する
-        _playerStatus.EquipWeapon.OffensivePower.Value += Damage;
+        _isUse = true;
 
-        // 防御、素早さを40%下げる。
-        _enemyStatus.EquipWeapon.CurrentOffensivePower -= _enemyStatus.EquipWeapon.OffensivePower * _subtractValue;
-        _enemyStatus.EquipWeapon.CurrentCriticalRate -= _enemyStatus.EquipWeapon.CriticalRate * _subtractValue;
-        _enemyStatus.EquipWeapon.CurrentWeaponWeight -= _enemyStatus.EquipWeapon.WeaponWeight * _subtractValue;
+        switch (_actor)
+        {
+            case ActorAttackType.Player:
+            {
+                // スキルの効果処理を実装する
+                _playerStatus.EquipWeapon.OffensivePower.Value += Damage;
 
-        _playerStatus.EquipWeapon.CurrentDurable.Value = 0;
+                // 防御、素早さを40%下げる。
+                _enemyStatus.EquipWeapon.CurrentOffensivePower -=
+                    _enemyStatus.EquipWeapon.OffensivePower * _subtractValue;
+                _enemyStatus.EquipWeapon.CurrentCriticalRate -=
+                    _enemyStatus.EquipWeapon.CriticalRate * _subtractValue;
+                _enemyStatus.EquipWeapon.CurrentWeaponWeight -=
+                    _enemyStatus.EquipWeapon.WeaponWeight * _subtractValue;
+
+                _playerStatus.EquipWeapon.CurrentDurable.Value = 0;
+            }
+                break;
+            case ActorAttackType.Enemy:
+            {
+                _enemyStatus.EquipWeapon.CurrentOffensivePower += Damage;
+
+                // 防御、素早さを40%下げる。
+                _playerStatus.EquipWeapon.OffensivePower.Value -=
+                    _playerStatus.EquipWeapon.OffensivePower.Value * _subtractValue;
+                _playerStatus.EquipWeapon.CriticalRate.Value -=
+                    _playerStatus.EquipWeapon.CriticalRate.Value * _subtractValue;
+                _playerStatus.EquipWeapon.WeaponWeight.Value -=
+                    _playerStatus.EquipWeapon.WeaponWeight.Value * _subtractValue;
+
+                _enemyStatus.EquipWeapon.CurrentDurable.Value = 0;
+            }
+                break;
+        }
     }
 
     public override void TurnEnd()
     {
-        _playerStatus.EquipWeapon.OffensivePower.Value -= Damage;
+        if (!_isUse)
+        {
+            return;
+        }
+
+        _isUse = false;
+        
+        switch (_actor)
+        {
+            case ActorAttackType.Player:
+                _playerStatus.EquipWeapon.OffensivePower.Value -= Damage;
+                break;
+            case ActorAttackType.Enemy:
+                _enemyStatus.EquipWeapon.CurrentOffensivePower -= Damage;
+                break;
+        }
+       
     }
 
     public override void BattleFinish()
     {
-
+        _isUse = false;
     }
 }
