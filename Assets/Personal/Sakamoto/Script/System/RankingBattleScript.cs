89fbb685cb206433988727c8816ed022419643e6
sakamotosyo <syo10150000@gmail.com>
2023-05-07 12:00:01 +0900

    [Update]HomeSceneでの機能を追加

--- Assets/Personal/Sakamoto/Script/System/RankingBattleScript.cs
+++ Assets/Personal/Sakamoto/Script/System/RankingBattleScript.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class RankingBattleScript : MonoBehaviour
 {
     [SerializeField] private EnemyDataBase _enemyDataBase;
     [SerializeField] private GameObject _buttonInsPos;
     [SerializeField] private GameObject _enemyPrefab;
+    [SerializeField] private GameObject _bossButtonObj;
+    [SerializeField] private Text _rankText;
     [SerializeField] private int _rankPointRange = 100;
     [SceneName]
     [SerializeField] private string _battleScene;
-    
+    private List<EnemyData> _enemyBossData = new();
+
+    private void Awake()
+    {
+        PlayerWeaponRecovery();
+    }
 
     void Start()
     {
         StartEnemySelect();
+        BossChallengeJudge();
     }
 
     void Update()
@@ -23,13 +32,13 @@ public class RankingBattleScript : MonoBehaviour
 
     }
 
-    public void StartEnemySelect() 
+    public void StartEnemySelect()
     {
-        if (GameManager.PlayerSaveData == null) 
+        if (GameManager.PlayerSaveData == null)
         {
             PlayerDataInit();
         }
-        EnemyData[] enemyDataHigh = _enemyDataBase.GetEnemyArrayData(GameManager.PlayerSaveData.PlayerRankPoint, _rankPointRange, true);
+        EnemyData[] enemyDataHigh = _enemyDataBase.GetRankEnemyArrayData(GameManager.PlayerSaveData.PlayerRankPoint, _rankPointRange, true);
         for (int i = 0; i < 3; i++)
         {
             var enemyButtonPrefab = Instantiate(_enemyPrefab);
@@ -45,25 +54,59 @@ public class RankingBattleScript : MonoBehaviour
             //button.onClick.AddListener(() => SceneLoader.LoadScene(_battleScene));
         }
 
-        EnemyData[] enemyDataLow = _enemyDataBase.GetEnemyArrayData(GameManager.PlayerSaveData.PlayerRankPoint, _rankPointRange, false);
-        for (int i = 0; i < 2; i++)
-        {
-            var enemyButtonPrefab = Instantiate(_enemyPrefab);
-            enemyButtonPrefab.transform.SetParent(_buttonInsPos.transform);
-            var randomIndex = Random.Range(0, enemyDataLow.Length);
-            //enemyButtonPrefab.GetComponent<Image>().sprite = enemyData[i].EnemySprite;
-            enemyButtonPrefab.GetComponent<Image>().sprite = enemyDataLow[randomIndex].EnemySprite;
-            var button = enemyButtonPrefab.GetComponent<Button>();
+        //EnemyData[] enemyDataLow = _enemyDataBase.GetRankEnemyArrayData(GameManager.PlayerSaveData.PlayerRankPoint, _rankPointRange, false);
+        //for (int i = 0; i < 2; i++)
+        //{
+        //    var enemyButtonPrefab = Instantiate(_enemyPrefab);
+        //    enemyButtonPrefab.transform.SetParent(_buttonInsPos.transform);
+        //    var randomIndex = Random.Range(0, enemyDataLow.Length);
+        //    //enemyButtonPrefab.GetComponent<Image>().sprite = enemyData[i].EnemySprite;
+        //    enemyButtonPrefab.GetComponent<Image>().sprite = enemyDataLow[randomIndex].EnemySprite;
+        //    var button = enemyButtonPrefab.GetComponent<Button>();
 
-            button.onClick.AddListener(() => SelectEnemy.SetImage(enemyButtonPrefab.GetComponent<Image>().sprite));
-            //button.onClick.AddListener(() => GameManager.SetEnemyData(enemyData[i]));
-            button.onClick.AddListener(() => GameManager.SetEnemyData(enemyDataLow[randomIndex]));
-            //button.onClick.AddListener(() => SceneLoader.LoadScene(_battleScene));
-        }
+        //    button.onClick.AddListener(() => SelectEnemy.SetImage(enemyButtonPrefab.GetComponent<Image>().sprite));
+        //    button.onClick.AddListener(() => GameManager.SetEnemyData(enemyDataLow[randomIndex]));
+        //}
     }
 
 
-    public void PlayerDataInit() 
+    public void BossChallengeJudge()
+    {
+        var enemyData = EnemyDataBase.BossSelect();
+        if (enemyData != null)
+        {
+            _enemyBossData.Add(enemyData);
+        }
+
+        if (_enemyBossData.Count != 0)
+        {
+            _bossButtonObj.SetActive(true);
+            var botton = _bossButtonObj.GetComponent<Button>();
+            botton.onClick.AddListener(() => ButtonBossSelect());
+        }
+    }
+
+    public void ButtonBossSelect()
+    {
+        GameManager.SetEnemyData(_enemyBossData[0]);
+        SceneLoader.LoadScene(_battleScene);
+    }
+
+    public void PlayerWeaponRecovery()
+    {
+        if (GameManager.PlayerSaveData == null) return;
+
+        var weaponDatas = GameManager.PlayerSaveData.WeaponArray;
+
+        for (int i = 0; i < weaponDatas.Length; i++)
+        {
+            weaponDatas[i].CurrentDurable = weaponDatas[i].MaxDurable;
+        }
+        GameManager.PlayerSaveData.WeaponArray = weaponDatas;
+        _rankText.text = GameManager.PlayerSaveData.PlayerRankPoint.ToString();
+    }
+
+    public void PlayerDataInit()
     {
         var playerData = new PlayerSaveData();
         var weaponDatas = new WeaponData[4];
