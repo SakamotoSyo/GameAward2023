9b47eb1c1e1557879f5518db21b0d3360fe7ca7d
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-05-16 18:44:17 +0900

    [update]因果応報スキル完成

--- Assets/Personal/Takai/Script/Skills/Spear/IngaouhouSkill.cs
+++ Assets/Personal/Takai/Script/Skills/Spear/IngaouhouSkill.cs
@@ -7,7 +7,12 @@ public class IngaouhouSkill : SkillBase
     private PlayableDirector _anim;
     private PlayerController _playerStatus;
     private EnemyController _enemyStatus;
-    
+    private const float ADD_VALUE = 0.2f;
+    private const int TURN = 4;
+    private int _count;
+    private int _turnCount;
+    private float _addValue;
+
     public IngaouhouSkill()
     {
         SkillName = "因果応報";
@@ -16,13 +21,13 @@ public class IngaouhouSkill : SkillBase
         Type = (SkillType)0;
         FlavorText = "発動したターンに攻撃を受けるとダメージを30%軽減し、反撃する。";
     }
-    
+
     private void Start()
     {
         _anim = GetComponent<PlayableDirector>();
     }
 
-    
+
     public override bool IsUseCheck(PlayerController player)
     {
         return true;
@@ -36,23 +41,51 @@ public class IngaouhouSkill : SkillBase
         _anim = GetComponent<PlayableDirector>();
         _anim.Play();
         SkillEffect();
-        await UniTask.WaitUntil(() => _anim.state == PlayState.Paused, cancellationToken: this.GetCancellationTokenOnDestroy());
+        await UniTask.WaitUntil(() => _anim.state == PlayState.Paused,
+            cancellationToken: this.GetCancellationTokenOnDestroy());
         Debug.Log("Anim End");
     }
 
     protected override void SkillEffect()
     {
         // スキルの効果処理を実装す
-
-
+        FluctuationStatusClass fluctuation;
+        _count++;
+        float value = _addValue;
+        _addValue = _playerStatus.PlayerStatus.EquipWeapon.CriticalRate.Value * ADD_VALUE;
+        fluctuation = new FluctuationStatusClass(0, 0, -value, 0, 0);
+        fluctuation = new FluctuationStatusClass(0, 0, _addValue, 0, 0);
     }
-    
+
     public override bool TurnEnd()
     {
-        return false;
+        if (_count > 0)
+        {
+            if (_turnCount <= TURN)
+            {
+                _turnCount++;
+            }
+            else
+            {
+                _count--;
+                _turnCount = 0;
+                float value = _addValue;
+                FluctuationStatusClass  fluctuation = new FluctuationStatusClass(0, 0, -value, 0, 0);
+                _addValue = _playerStatus.PlayerStatus.EquipWeapon.CriticalRate.Value * ADD_VALUE;
+                if (_count <= 0)
+                {
+                    _count = 0;
+                    _addValue = 0;
+                }
+            }
+        }
+
+        return true;
     }
 
     public override void BattleFinish()
     {
+        _turnCount = 0;
+        _addValue = 0;
     }
 }
