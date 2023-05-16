635288497a44ab9fa311cbd96d80988e0c33287f
TakaiKentarou <mmpm664@yahoo.co.jp>
2023-05-16 18:48:27 +0900

    [fix]因果応報スキル修正

--- Assets/Personal/Takai/Script/Skills/Spear/IngaouhouSkill.cs
+++ Assets/Personal/Takai/Script/Skills/Spear/IngaouhouSkill.cs
@@ -7,11 +7,6 @@ public class IngaouhouSkill : SkillBase
     private PlayableDirector _anim;
     private PlayerController _playerStatus;
     private EnemyController _enemyStatus;
-    private const float ADD_VALUE = 0.2f;
-    private const int TURN = 4;
-    private int _count;
-    private int _turnCount;
-    private float _addValue;
 
     public IngaouhouSkill()
     {
@@ -49,43 +44,16 @@ public class IngaouhouSkill : SkillBase
     protected override void SkillEffect()
     {
         // スキルの効果処理を実装す
-        FluctuationStatusClass fluctuation;
-        _count++;
-        float value = _addValue;
-        _addValue = _playerStatus.PlayerStatus.EquipWeapon.CriticalRate.Value * ADD_VALUE;
-        fluctuation = new FluctuationStatusClass(0, 0, -value, 0, 0);
-        fluctuation = new FluctuationStatusClass(0, 0, _addValue, 0, 0);
+        
     }
 
     public override bool TurnEnd()
     {
-        if (_count > 0)
-        {
-            if (_turnCount <= TURN)
-            {
-                _turnCount++;
-            }
-            else
-            {
-                _count--;
-                _turnCount = 0;
-                float value = _addValue;
-                FluctuationStatusClass  fluctuation = new FluctuationStatusClass(0, 0, -value, 0, 0);
-                _addValue = _playerStatus.PlayerStatus.EquipWeapon.CriticalRate.Value * ADD_VALUE;
-                if (_count <= 0)
-                {
-                    _count = 0;
-                    _addValue = 0;
-                }
-            }
-        }
-
-        return true;
+        
+        return false;
     }
 
     public override void BattleFinish()
     {
-        _turnCount = 0;
-        _addValue = 0;
     }
 }
