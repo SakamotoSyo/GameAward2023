fe6ce31cc5c1050050d3ce59b06ac534013e6a6b
Akira Watanabe <a0323w@gmail.com>
2023-05-25 16:32:59 +0900

    [update] MeshSceneのUIテスト

---
+++ Assets/Personal/Watanabe/Scripts/SelectWeapon.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectWeapon : MonoBehaviour
+{
+    [SerializeField] private Image _outLineImage = default;
+
+    private bool _isActive = false;
+
+    private void Start()
+    {
+        _outLineImage.gameObject.SetActive(false);
+        _isActive = false;
+    }
+
+    public void SelectThis()
+    {
+        _outLineImage.gameObject.SetActive(!_isActive);
+
+        _isActive = _outLineImage.gameObject.activeSelf;
+    }
+}
