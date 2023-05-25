50ec9396cefdd50ee1a5b05bbf67fc49061d2d20
RK <riku.kuu0421@gmail.com>
2023-05-25 17:00:32 +0900

    [add]敵のHPバーを作成

--- Assets/Personal/Ohashi/Script/Enemy/EnemyView.cs
+++ Assets/Personal/Ohashi/Script/Enemy/EnemyView.cs
@@ -6,11 +6,25 @@ public class EnemyView : MonoBehaviour
     [SerializeField, Tooltip("Helthを表示するテキスト")]
     private Text _healthText;
 
+    [SerializeField]
+    private Text _maxHealthText;
+
+    [SerializeField]
+    private Image _currentHealthImage;
+
     /// <summary>
     /// エネミーの残り体力をテキストに表示する
     /// </summary>
-    public void HealthText(float health)
+    public void HealthText(float health, float max)
     {
         _healthText.text = health.ToString("00");
+        _currentHealthImage.fillAmount = health / max;
     }
+
+    public void MaxHealthText(float max)
+    {
+        _maxHealthText.text = max.ToString("00");
+    }
+
+
 }
