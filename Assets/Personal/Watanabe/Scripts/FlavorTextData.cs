4c44cb3758f66e4732471dbfb0e3b3483e931578
Akira Watanabe <a0323w@gmail.com>
2023-05-26 22:19:18 +0900

    [update] TitleScene更新(これからFlavorText)

---
+++ Assets/Personal/Watanabe/Scripts/FlavorTextData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlavorTextData : MonoBehaviour
+{
+    [Multiline(5)]
+    [SerializeField] private string _flavorText = "";
+
+    public string flavorText => _flavorText;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
