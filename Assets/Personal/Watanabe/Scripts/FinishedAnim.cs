4b5e2167da4fccbd5cfa6eee2d85bf2c81395ed5
Akira Watanabe <a0323w@gmail.com>
2023-05-27 13:40:05 +0900

    [update] TitleSceneの動き

---
+++ Assets/Personal/Watanabe/Scripts/FinishedAnim.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FinishedAnim : MonoBehaviour
+{
+    [SerializeField] private GameObject _button = default;
+
+    public void FinishedSetLogo()
+    {
+        _button.SetActive(true);
+    }
+}
---
+++ Assets/Personal/Watanabe/Scripts/TitleScene.cs
@@ -0,0 +1,21 @@
+﻿using CriWare;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TitleScene : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _onPlayAnim = default;
+    [SerializeField] private CriAtomSource _atomSource = default;
+
+    private void Start()
+    {
+        _atomSource.volume = 0.5f;
+        SoundManager.Instance.CriAtomBGMPlay("BGM_Title");
+    }
+
+    public void PlayAnim()
+    {
+        _onPlayAnim?.Invoke();
+        _atomSource.volume = 1f;
+    }
+}
